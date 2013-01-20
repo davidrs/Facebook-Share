f858c06943a49ffe743a53725ec4d0c1bbae9006
David RS <david@smewebsites.com>
2013-01-20 17:21:51 -0500

    +Added twitter support ~renamed files to reflect dual twitter and fb support.

--- php/demo.php
+++ php/demo.php
@@ -10,19 +10,21 @@
 	/*
 	  $appId		// id for your app. (Get one by registering your domain as an app at http://developers.facebook.com) 
 	  $link			// Link to share
+	  
+	OPTIONAL INPUTS: Facebook does its best to fill in from the website's meta tags and content.
 	  $picture		// OPTIONAL: Picture to show in the share dialogue. 
 	  $title		// OPTIONAL: The title of the Facebook post.
 	  $caption		// OPTIONAL: The caption/sub-title of the Facebook post.
 	  $description	// OPTIONAL: Body text for share dialogue
 	  $redirect		// OPTIONAL: Where to redirect the sharer after sharing. uses the $ink url if not provided. 
 	  
-	  OPTIONAL: Facebook does its best to fill in OPTIONAL tags from the website's meta tags and content.
+	  $twitterText	//OPTIONAL: text to go with link on Tweet
 	  
 	 // OUTPUT: 
 		string: url for sharing
 	*/
 
-	include 'generateFB.php';
+	include 'generateSocialLinks.php';
 
 	//inputs
 	$appId="582181088462989"; 				//id for your app. (You must register your domain as an app at http://developers.facebook.com) 
@@ -31,12 +33,27 @@
 	$title='Radi-Aid: The Game';	//The title of the Facebook post.
 	$caption='Web & Mobile Friendly'; //The caption/sub-title of the Facebook post.
 	$description='Help Santa deliver radiators to frozen Norwegians in this un-official game for Africa for Norway. Web & Mobile friendly.';
-	$redirect= 'http://davidrs.com/wp/my-projects/radi-aid-game/'; // I re-use the link url, but you could redirect the sharer somewhere different.  
-
-
-	//get and print the link string.
+	$redirect= 'http://davidrs.com/wp/my-projects/radi-aid-game/'; // I re-use the link url, but you could redirect the sharer somewhere different.
+	  
+	$twitterText= 'My twitter text';
+	
+	
+	//get and print the facebook string.
+	echo '<h2>Facebook Share Link: generateFacebookShare($appId, $link,  $title,  $caption, $picture, $description, $redirect)</h2>';
 	echo generateFacebookShare($appId, $link,  $title,  $caption, $picture, $description, $redirect);
+	
+	echo '<hr />';
+	//get and print the twitter string.
+	echo '<h2>Twitter Share Link: generateTwitterShare( $link,  $twitterText)</h2>';
+	echo generateTwitterShare( $link,  $twitterText);
 
+	echo '<hr />';
+	//Get both links at once, twitter uses the the '$title' for the text
+	echo '<h2>Get both links at once with generateSocialLinks(...):</h2>';
+	$socialLinks=generateSocialLinks($appId, $link,  $title,  $caption, $picture, $description, $redirect);
+	echo '<a href="'.$socialLinks['facebook'].'" >'  .$socialLinks['facebook']. '</a>';
+	echo '<hr />';
+	echo '<a href="'.$socialLinks['twitter'].'" >'  .$socialLinks['twitter']. '</a>';
 
 
 ?>
